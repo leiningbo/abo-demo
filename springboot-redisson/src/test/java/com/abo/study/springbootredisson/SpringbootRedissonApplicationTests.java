28c7f638d59ba54b1d71bca1024f5286cbd8193b
zuoyafei <zuoyafei@haizitong.com>
2021-02-24 11:14:05 +0800

    'redisson'

---
+++ springboot-redisson/src/test/java/com/abo/study/springbootredisson/SpringbootRedissonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abo.study.springbootredisson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRedissonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
