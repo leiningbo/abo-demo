ca67acc69db49ea154e4750a74f7c2f248f36169
zuoyafei <zuoyafei@haizitong.com>
2021-02-24 14:09:17 +0800

    demo test

---
+++ demo/src/test/java/com/abo/demo/DemoStudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.abo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
+    }
+
+}
