019437d8c2619fc0fe8d9a2965968a361cd5c0a5
zuoyafei <zuoyafei@haizitong.com>
2021-02-24 14:44:37 +0800

    test

--- springboot-redisson/src/test/java/com/abo/study/SpringbootRedissonApplicationTests.java
+++ springboot-redisson/src/test/java/com/abo/study/SpringbootRedissonApplicationTests.java
@@ -1,13 +1,19 @@
 package com.abo.study;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith( SpringRunner.class )
 @SpringBootTest
-class SpringbootRedissonApplicationTests {
+public class SpringbootRedissonApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void test() {
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
     }
 
 }
--- springboot-websocket/src/test/java/com/abo/study/springbootwebsocket/SpringbootWebsocketApplicationTests.java
+++ springboot-websocket/src/test/java/com/abo/study/springbootwebsocket/SpringbootWebsocketApplicationTests.java
@@ -5,10 +5,13 @@ import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootWebsocketApplicationTests {
+public class SpringbootWebsocketApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void test() {
+        int a = 1;
+        int b = 2;
+        System.out.println(a + b);
     }
 
 }
