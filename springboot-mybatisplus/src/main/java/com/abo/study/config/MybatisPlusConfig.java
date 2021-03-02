7eba048654766b76601a41d30fb13c21d45f4ea0
leiningbo <leiningbo@haizitong.com>
2021-03-02 11:19:44 +0800

    mybatis-plus代码生成器，有点拉跨

---
+++ springboot-mybatisplus/src/main/java/com/abo/study/config/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package com.abo.study.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 分页插件
+ * @author lnb
+ * @date 2021/3/2 10:43
+ * @description
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
