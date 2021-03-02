9a6fc757c5e169dc6f35bfb90de29e999e51e45f
leiningbo <leiningbo@haizitong.com>
2021-03-02 11:50:44 +0800

    redisson分布式锁

---
+++ springboot-redisson/src/main/java/com/abo/study/controller/DistributedRedisLockController.java
@@ -0,0 +1,60 @@
+package com.abo.study.controller;
+
+import com.abo.study.utils.RedisUtils;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Redisson分布式锁
+ * @author lnb
+ * @date 2021/2/24 11:41
+ * @describe
+ */
+@RestController
+@RequestMapping("/redis")
+public class DistributedRedisLockController {
+
+    private static final String STOCK_KEY = "stock";
+
+    @Autowired
+    private Redisson redisson;
+    @Autowired
+    private RedisUtils redisUtils;
+
+    @RequestMapping(value = "/setDefaultStock")
+    public String setDefaultStock() {
+        String stockNumber = "200";
+        redisUtils.setEx(STOCK_KEY, stockNumber, 1, TimeUnit.HOURS);
+        return "初始化库存：" + redisUtils.get(STOCK_KEY);
+    }
+
+    /**
+     * 减库存
+     */
+    @RequestMapping( value = "/reduceStock", method = RequestMethod.POST )
+    public String reduceStock() {
+        RLock lock = redisson.getLock("/payOrder");
+        try {
+            lock.lock();
+            int stock = Integer.parseInt(redisUtils.get(STOCK_KEY));
+            if (stock > 0) {
+                int reduceStock = stock - 1;
+                redisUtils.setEx(STOCK_KEY, reduceStock + "", 1, TimeUnit.HOURS);
+                System.out.println("减库成功，剩余库存：" + stock);
+            } else {
+                System.out.println("减库失败，库存不足");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+        return "支付成功";
+    }
+}
