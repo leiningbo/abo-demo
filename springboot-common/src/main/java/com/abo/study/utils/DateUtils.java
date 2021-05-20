a05b005674bcb00d3479febf89d17b4183a99102
leiningbo <leiningbo@haizitong.com>
2021-05-20 11:45:22 +0800

    DateUtils

--- springboot-common/src/main/java/com/abo/study/utils/DateUtils.java
+++ springboot-common/src/main/java/com/abo/study/utils/DateUtils.java
@@ -1,7 +1,11 @@
 package com.abo.study.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * @author lnb
@@ -36,6 +40,60 @@ public class DateUtils {
             default:
                 return 0;
         }
+    }
+
+    /**
+     * 获得近几个月的开始和结束时间
+     * month 1:表示当月
+     * 例如：2021-05-01 00:00:00  2021-05-31 23:59:59
+     */
+    public static Map<String, Date> getStartToEndTimeByMonth(int month) throws ParseException {
+        Map<String,Date> dateMap = new LinkedHashMap<>();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar start = Calendar.getInstance();
+        start.add(Calendar.MONTH, -(month - 1));
+        start.set(Calendar.DAY_OF_MONTH, 1);
+        String startTime = sdf.format(start.getTime()) + " 00:00:00";
+        dateMap.put("startTime", simpleDateFormat.parse(startTime));
+
+        // 当月最后一天
+        Calendar end = Calendar.getInstance();
+        end.set(Calendar.DAY_OF_MONTH, end.getActualMaximum(Calendar.DAY_OF_MONTH));
+        String endTime = sdf.format(end.getTime()) + " 23:59:59";
+        dateMap.put("endTime", simpleDateFormat.parse(endTime));
+        return dateMap;
+    }
+
+    public static Map<String, Date> getLastYearAugustToThisYearJuly(int month) throws ParseException {
+        Map<String,Date> dateMap = new LinkedHashMap<>();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        // 去年8月第一天
+        Calendar start = Calendar.getInstance();
+        if (month < 8) {
+            start.add(Calendar.YEAR, -1);
+        }
+        start.add(Calendar.MONTH, 7);
+        start.set(Calendar.DAY_OF_MONTH, 1);
+        String startTime = sdf.format(start.getTime()) + " 00:00:00";
+        dateMap.put("startTime", simpleDateFormat.parse(startTime));
+
+        // 今年7月最后一天
+        Calendar end = Calendar.getInstance();
+        if (month > 7) {
+            end.add(Calendar.YEAR, 1);
+        }
+        end.set(Calendar.MONTH, 6);
+        end.set(Calendar.DAY_OF_MONTH, end.getActualMaximum(Calendar.DAY_OF_MONTH));
+        String endTime = sdf.format(end.getTime()) + " 23:59:59";
+        dateMap.put("endTime", simpleDateFormat.parse(endTime));
+        return dateMap;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Map<String, Date> threeMonthStartToEndTime = getLastYearAugustToThisYearJuly(8);
+
     }
 
 }
